a95b726bbbaf7c8461ab61bd1ad5d03eb6915a84
LuisDLF98 <ldelafuente34@gmail.com>
2021-08-26 09:36:01 -0500

    Completed tic_tac_toe

--- oop_basics/tic_tac_toe.rb
+++ oop_basics/tic_tac_toe.rb
@@ -1,7 +1,7 @@
 class Tictactoe
-    attr_accessor :grid, :positions, :current_turn
+    attr_accessor :grid, :positions, :current_symbol, :current_player
 
-    def initialize(first_player = false)
+    def initialize(player_symbol = false)
         @grid = Array.new(3) { Array.new(3) {" "}}
         @positions = {
             "a1" => [0,0],
@@ -14,53 +14,117 @@ class Tictactoe
             "c2" => [2,1],
             "c3" => [2,2],
         }
-        @current_turn = first_player #boolean false is "x", true is "o"
+        @current_symbol = player_symbol # Boolean false is "X", true is "O"
+        @current_player = 1
     end
 
-    def take_turn(move)
-       if @current_turn
-         @grid[@positions[move][0]][@positions[move][1]] = "O"
+    # Places the current player's move onto the board
+    def take_turn(position)
+       if @current_symbol
+         @grid[@positions[position][0]][@positions[position][1]] = "O"
        else
-         @grid[@positions[move][0]][@positions[move][1]] = "X" 
+         @grid[@positions[position][0]][@positions[position][1]] = "X" 
        end
     end
 
+    # Print the game board into the terminal
     def print
         puts "  |1|2|3"
         puts "--------"
         puts "a |#{@grid[0][0]}|#{@grid[0][1]}|#{@grid[0][2]}"
+        puts "--------"
         puts "b |#{@grid[1][0]}|#{@grid[1][1]}|#{@grid[1][2]}"
+        puts "--------"
         puts "c |#{@grid[2][0]}|#{@grid[2][1]}|#{@grid[2][2]}"
     end
 
+    # Checks if a move is valid
+    # Valid move is one that is one the board and not already taken
     def valid(position)
-        @grid[@positions[position][0]][@positions[position][1]].eql?(" ")
+        @positions.has_key?(position) && @grid[@positions[position][0]][@positions[position][1]].eql?(" ")
     end
 
+    def player_won
+        self.print
+        puts "Player #{@current_player} won!"
+        return true
+    end
+
+    # Check is the current player won the game
+    def is_win?
+        if @grid.any? { |row| row.all? { |space| space == "X" } } || @grid.any? { |row| row.all? { |space| space == "O" } }
+            # Check the rows of the board
+
+            return self.player_won
+        elsif @grid[0][0] != " " && @grid[0][0] == @grid[1][0] && @grid[0][0] == @grid[2][0]
+            # Check the first column of the board
+            
+            return self.player_won
+        elsif @grid[0][1] != " " && @grid[0][1] == @grid[1][1] && @grid[0][1] == @grid[2][1]
+            # Check the second column of the board
+
+            return self.player_won
+        elsif @grid[0][2] != " " && @grid[0][2] == @grid[1][2] && @grid[0][2] == @grid[2][2]
+            # Check the third column of the board
+
+            return self.player_won
+        elsif @grid[0][0] != " " && @grid[0][0] == @grid[1][1] && @grid[0][0] == @grid[2][2]
+            # Check the right diagonal of the board
+
+            return self.player_won
+        elsif @grid[0][2] != " " && @grid[0][2] == @grid[1][1] && @grid[0][2] == @grid[2][0]
+            # Check the left diagonal of the board
+
+            return self.player_won
+        else
+            return false
+        end
+    end
+
+    def is_tie?
+        if @grid.all? { |row| row.none? { |space| space == " " } }
+            self.print
+            puts "The players tied!"
+            return true
+        else 
+            return false
+        end
+    end
+
+    # Start up the game
     def start
         loop do
-            puts "Select a position"
+            self.print
+
+            puts "Player #{@current_player} select a position"
             input = gets.chomp
 
             # Loop until the player inputs an open position
             until valid(input) do
-                puts "Select a position"
+                puts "Player #{@current_player} select a position"
                 input = gets.chomp
             end
             
             take_turn(input)
-            @current_turn = !@current_turn
+            break if self.is_win? || self.is_tie?
 
-            self.print
+            @current_symbol = !@current_symbol
+            @current_player = (@current_player % 2) + 1
         end
     end
 end
 
-puts "Is the first player x or o?"
+puts "Player 1 choose a valid symbol (X or O)"
 input = gets.chomp
 
+# Loop until the player inputs an valid symbol
+until input.upcase == "X" || input.upcase == "O" do
+    puts "Player 1 select a valid symbol"
+    input = gets.chomp
+end
+
 isX = false
-isX = true if input.eql?("o")
+isX = true if input.upcase.eql?("O")
 
 game = Tictactoe.new(isX)
 game.start
