aa7299bdd7995222278a80bd0a7cda9597cda801
LuisDLF98 <ldelafuente34@gmail.com>
2021-08-30 09:22:15 -0500

    Created my_each_with_index enumerable

--- custom_enumerables/custom_enumerables.rb
+++ custom_enumerables/custom_enumerables.rb
@@ -4,11 +4,22 @@ module Enumerable
             yield(self[i])
         end
     end
+
+    def my_each_with_index
+        for i in (0...self.length) do 
+            yield(self[i], i)
+        end
+    end
 end
 
 
 
-puts "my_each vs. each"
 numbers = [1, 2, 3, 4, 5]
+
+puts "my_each vs. each"
 numbers.my_each { |item| puts item }
 numbers.each { |item| puts item }
+
+puts "my_each_with_index vs. each_with_index"
+numbers.my_each_with_index { |item, index| puts "numbers[#{index}] = #{item}" }
+numbers.each_with_index { |item, index| puts "numbers[#{index}] = #{item}" }
